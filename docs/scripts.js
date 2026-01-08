c72c7d3d6442acae62cc79f3f17a8c69025fdc70
EzeGamer135 <ezesouto2@gmail.com>
2026-01-08 00:04:21 -0300

    update 2

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,26 +1,10 @@
-<script>
-    function updateNavbarMode() {
-        const ratio = window.innerWidth / window.innerHeight;
+const menuBtn = document.getElementById("menuBtn");
+const overlay = document.getElementById("overlay");
 
-        if (ratio <= 4 / 3) {
-            document.body.classList.add("compact-nav");
-        } else {
-            document.body.classList.remove("compact-nav", "menu-open");
-        }
-    }
+menuBtn.addEventListener("click", () => {
+    document.body.classList.toggle("menu-open");
+});
 
-    const menuBtn = document.getElementById("menuBtn");
-    const overlay = document.getElementById("overlay");
-
-    menuBtn.addEventListener("click", () => {
-        document.body.classList.toggle("menu-open");
-    });
-
-    overlay.addEventListener("click", () => {
-        document.body.classList.remove("menu-open");
-    });
-
-    window.addEventListener("resize", updateNavbarMode);
-    window.addEventListener("load", updateNavbarMode);
-
-</script>
+overlay.addEventListener("click", () => {
+    document.body.classList.remove("menu-open");
+});
